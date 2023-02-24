16c9145f4732db0abb8f2f0536e2a8d199d76422
AthenasCode <emoore5203@gmail.com>
2023-02-24 12:07:17 +0800

    cra
    create-react-app

---
+++ expenses/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Expenses Chart Component</header>
+    </div>
+  );
+}
+
+export default App;
